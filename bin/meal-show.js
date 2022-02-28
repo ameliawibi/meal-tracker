66d8117fb208db00f0b033ba56c7e60f82b526e4
Amelia Wibisono <ameliawibi@gmail.com>
2022-02-28 22:55:56 +0800

    past week

--- bin/meal-show.js
+++ bin/meal-show.js
@@ -9,4 +9,16 @@ program.command("filter").description("Show filtered").action(show.showFilter);
 
 program.command("drink").description("Drink stats").action(show.showDrink);
 
+program
+  .command("week-so-far")
+  .description("Get a list of all meals since the week began")
+  .action(show.showWeekSoFar);
+
+program
+  .command("past-week")
+  .description(
+    "Get a list of the meals in the past seven days not including today"
+  )
+  .action(show.showPastWeek);
+
 program.parse(process.argv);
--- commands/show.js
+++ commands/show.js
@@ -2,6 +2,16 @@ import inquirer from "inquirer";
 import colors from "colors";
 import { client, whenQueryDone } from "../index.js";
 
+const now = new Date().toISOString().slice(0, 19).replace("T", " ");
+const lastWeek = new Date(new Date() - 1000 * 60 * 60 * 24 * 7)
+  .toISOString()
+  .slice(0, 19)
+  .replace("T", " ");
+const yesterday = new Date(new Date() - 1000 * 60 * 60 * 24 * 1)
+  .toISOString()
+  .slice(0, 19)
+  .replace("T", " ");
+
 export class Show {
   showAll = async () => {
     let sqlQuery = "SELECT * FROM meals;";
@@ -40,6 +50,14 @@ export class Show {
   showDrink = async () => {
     let sqlQuery =
       "SELECT type,COUNT(*) AS nbr, (AVG(amount_of_alcohol)) AS avg_alcohol FROM meals GROUP BY type ORDER BY avg_alcohol DESC;";
+    client.query(sqlQuery, whenQueryDone);
+  };
+  showWeekSoFar = async () => {
+    let sqlQuery = `SELECT * FROM meals WHERE created_at BETWEEN '${lastWeek}' AND '${now}';`;
+    client.query(sqlQuery, whenQueryDone);
+  };
+  showPastWeek = async () => {
+    let sqlQuery = `SELECT * FROM meals WHERE created_at BETWEEN '${lastWeek}' AND '${yesterday}';`;
     client.query(sqlQuery, whenQueryDone);
   };
 }
